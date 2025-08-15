7f3364a05469bf4612ac1b04f54eb9a10365c33e
Mahendra Singh Choudhary <cmahendra9999@gmail.com>
2025-08-15 20:16:57 +0530

    history in admin

--- event_scheduling_system/user/admin.py
+++ event_scheduling_system/user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Organizer, Customer
+from .models import Organizer, Customer, HistoryPoint
 
 
 class OrganizerAdmin(admin.ModelAdmin):
@@ -15,6 +15,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
     search_fields = ('user__username', 'user__email')
 
+class HistoryPointAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'content_type', 'object_id')
+    list_filter = ('action', 'created_at')
+    search_fields = ('user__username', 'content_type__model')
+
 # Register the models
 admin.site.register(Organizer, OrganizerAdmin)
 admin.site.register(Customer, CustomerAdmin)
+admin.site.register(HistoryPoint, HistoryPointAdmin)
